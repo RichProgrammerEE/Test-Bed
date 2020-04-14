3a6afa6099287419a1aaee64c1cfd528518f9421
Cole Richardson <crichardson@3db-labs.com>
2020-04-14 08:13:18 -0400

    Threading example

--- testBed/src/main.cpp
+++ testBed/src/main.cpp
@@ -1,44 +1,72 @@
 #include <iostream>
-#include <memory>
 #include <thread>
-#include <chrono>
-#include <mutex>
+#include <algorithm>
 
-struct Base {
-    Base() { std::cout << "  Base::Base()\n"; }
-    // Note: non-virtual destructor is OK here
-    ~Base() { std::cout << "  Base::~Base()\n"; }
-};
-
-struct Derived : public Base {
-    Derived() { std::cout << "  Derived::Derived()\n"; }
-    ~Derived() { std::cout << "  Derived::~Derived()\n"; }
-};
-
-void thr(std::shared_ptr<Base> p) {
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::shared_ptr<Base> lp = p; // thread-safe, even though the
-                                  // shared use_count is incremented
-    {
-        static std::mutex io_mutex;
-        std::lock_guard<std::mutex> lk(io_mutex);
-        std::cout << "local pointer in a thread:\n"
-                  << "  lp.get() = " << lp.get() << ", lp.use_count() = " << lp.use_count() << '\n';
+void thread_function() {
+    for(size_t i = 0; i < 100; ++i) {
+        std::cout << "Thread (ID: " << std::this_thread::get_id();
+        std::cout << ") function is executing!" << std::endl;
     }
 }
 
-int main() {
-    std::shared_ptr<Base> p = std::make_shared<Derived>();
+class WorkerThread {
+public:
+    void operator()() {
+        for(size_t i = 0; i < 10; ++i) {
+            std::cout << "Thread (ID: " << std::this_thread::get_id();
+            std::cout << ") functor override is executing!" << std::endl;
+        }
+    }
+};
 
-    std::cout << "Created a shared Derived (as a pointer to Base)\n"
-              << "  p.get() = " << p.get() << ", p.use_count() = " << p.use_count() << '\n';
-    std::thread t1(thr, p), t2(thr, p), t3(thr, p);
-    p.reset(); // release ownership from main
-    std::cout << "Shared ownership between 3 threads and released\n"
-              << "ownership from main:\n"
-              << "  p.get() = " << p.get() << ", p.use_count() = " << p.use_count() << '\n';
-    t1.join();
-    t2.join();
-    t3.join();
-    std::cout << "All threads completed, the last one deleted Derived\n";
+void newThreadCallback(int* p) {
+    std::cout << "Inside Thread :  "
+                 " : p = "
+              << *p << std::endl;
+    std::chrono::milliseconds dura(1000);
+    std::this_thread::sleep_for(dura);
+    *p = 19;
+}
+
+void startNewThread() {
+    int i = 10;
+    std::cout << "Inside Main Thread :  "
+                 " : i = "
+              << i << std::endl;
+    std::thread t(newThreadCallback, &i);
+    t.detach();
+    std::cout << "Inside Main Thread :  "
+                 " : i = "
+              << i << std::endl;
+}
+
+int main() {
+    // std::thread thread1(thread_function);
+    // std::thread thread2((WorkerThread()));
+    // std::thread thread3([]() {
+    //     for(size_t i = 0; i < 10; ++i) {
+    //         std::cout << "Thread (ID: " << std::this_thread::get_id();
+    //         std::cout << ") with Lambda is executing!" << std::endl;
+    //     }
+    // });
+
+    std::vector<std::thread> threadPool;
+    for(size_t i = 0; i < 10; ++i)
+        threadPool.push_back(std::thread((WorkerThread())));
+
+    for(size_t i = 0; i < 10; ++i)
+        std::cout << "Main thread is executing!" << std::endl;
+
+    // Now wait for all the threads in the pool to finish by call
+    // join() on each std::thread object
+    std::for_each(threadPool.begin(), threadPool.end(), std::mem_fn(&std::thread::join));
+
+    // //  This example write to memory that is no longer in scope!
+    // startNewThread();
+    // std::chrono::milliseconds dura( 2000 );
+    // std::this_thread::sleep_for( dura );
+    // return 0;
+
+    std::cout << "Exiting main function!" << std::endl;
+    return 0;
 }
