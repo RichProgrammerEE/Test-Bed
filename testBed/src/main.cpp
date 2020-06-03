010881a3ecb6ebca7bcccb3a46673e1659aad881
Cole Richardson <crichardson@3db-labs.com>
2020-06-03 08:23:57 -0400

    std::condition_variable example

--- testBed/src/main.cpp
+++ testBed/src/main.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
 #include <thread>
 #include <algorithm>
+#include <mutex>
+#include <condition_variable>
 
-class Test {
-    public:
-    double x{ 0.0 };
-};
+bool is_ready{ false };
+std::mutex m;
+std::condition_variable cv;
+
+void func()
+{
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    std::unique_lock<std::mutex> lk(m);
+    is_ready = true;
+    cv.notify_one();
+}
 
 int main() {
-    // This makes more sense if you read this right to left
-    Test * const test = new Test(); // Const Pointer
-    Test const * test2 = new Test(); // Pointer to const object
-    const Test * test3 = new Test(); // Pointer to const object
-    test->x = 10;
-    //test = test2;
-    //test3->x = 10;
-
-    delete test;
-    delete test2;
-    delete test3;
-
+    std::thread t(func);
+    std::unique_lock<std::mutex> lk(m);
+    while(!is_ready) {
+        cv.wait(lk);
+        if(!is_ready)
+            std::cout << "Spurious wake up!\n";
+    }
+    t.join();
     std::cout << "Exiting main function!" << std::endl;
     return 0;
 }
