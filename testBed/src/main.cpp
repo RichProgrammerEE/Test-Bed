57296f95f3e1f9665e2842b5c1c1ddd23c27a7e1
Cole Richardson <crichardson@3db-labs.com>
2020-10-07 13:04:37 -0400

    Out of order instructions example without mutex.

--- testBed/src/main.cpp
+++ testBed/src/main.cpp
@@ -1,39 +1,47 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <thread>
+
 using namespace std;
 
-void SplitString(string s, vector<string> &v){
-	
-	string temp = "";
-	for(int i=0;i<s.length();++i){
-		
-		if(s[i]==' '){
-			v.push_back(temp);
-			temp = "";
-		}
-		else{
-			temp.push_back(s[i]);
-		}
-		
-	}
-	v.push_back(temp);
-	
+void incrementA(int* a)
+{
+    int tmp = *a;
+    tmp = tmp + 1;
+    *a = tmp;
 }
 
-template<typename T>
-void PrintVector(vector<T> v){
-	for(int i=0;i<v.size();++i)
-		cout<<v[i]<<endl;
-	cout<<"\n";
+void changeA(int* a)
+{
+    if(*a == -1)
+        *a = 10;
 }
 
 int main()
 {
-	vector<string> v;
-	string s = "Hello! My name is John Smith.";
-	cout<<"Input String: "<<endl<<s<<endl;
-	SplitString(s, v);
-	cout<<"String After Split"<<endl;
-	PrintVector(v);
-	
+    vector<size_t> results{ 0, 0, 0, 0};
+    for(size_t i = 0; i < 10000; ++i) {
+        int a = -1;
+        std::thread second(changeA, &a);
+        std::thread first(incrementA, &a);
+        // synchronize threads:
+        first.join();
+        second.join();
+        if(a == 0)
+            results[0] += 1;
+        else if(a == 10)
+            results[1] += 1;
+        else if(a == 11)
+            results[2] += 1;
+        else
+            results[3] += 1;
+    }
+
+    cout << "Value of A:\n"
+         << "0: " << results[0] << " times" << "\n"
+         << "10: " << results[1] << " times" << "\n"
+         << "11: " << results[2] << " times" << "\n"
+         << "Unknown: " << results[3] << " times" << endl;
+
+    return 0;
 }
